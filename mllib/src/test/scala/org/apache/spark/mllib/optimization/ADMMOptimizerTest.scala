ajtulloch/incubator-spark

[MLlib] Generic ADMM implementation (with L1-regularized logistic regression example)
Summary:

This diff implements the Alternating Direction Method of
Multipliers (ADMM) for solving distributed convex optimization problems
in MLlib, following Boyd et. al. in
http://www.stanford.edu/~boyd/papers/pdf/admm_distr_stats.pdf.

The core is a generic optimization driver at
mllib/optimization/ADMMOptimizer.scala, which takes an instance of an
ADMMUpdater.  Individual inference algorithms (sparse logistic
regression, lasso regression, SVMs, etc) implement the ADMM updater
interface.

This diff includes an implementation of this interface for sparse
logistic regression, following the equations given in (8.2.2) of Boyd
et. al.  To solve the L^2 regularized logistic regression problem
subproblem in the , we
use the implementation of L-BFGS given in ScalaNLP/breeze (Apache 2.0
licensed).

Implementing other inference algorithms (e.g. SVM as in 8.2.3 of Boyd
et. al) is trivial - see https://github.com/ajtulloch/admmlrspark/
for (preliminary) implementations of SVM and lasso regression.

This implementation isn't heavily optimized for performance - though
that should be easy to improve - although each component is fairly well
tested.

I'm not sure if anyone is working on ADMM in Spark (it seems like a nice
generic optimization procedure to have in the framework), but if this is
already being taken care of, or someone else would prefer to take this
on, let me know.

TODO

- Further testing, performance improvements
- Algorithmic improvements (over-relaxation, multiple (x, u) updates
per z update, etc from Section (3.4) in Boyd et. al.
- Implement and Java, Python bindings.

NB: this is the first non-trivial Scala/Java code I've ever written, so
it's most likely unidiomatic, unperformant, etc.

Test Plan:
- QuickCheck verification of most of the pure functions, and
verification that convexity etc holds for the objective/gradient
functions in the sparse LR case.

- QuickCheck verifc

mllib/src/test/scala/org/apache/spark/mllib/optimization/ADMMOptimizerTest.scala
@@ -1,0 +1,54 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.mllib.classification
+
+import org.apache.spark.SparkContext
+import org.apache.spark.mllib.optimization.ADMMState
+import org.apache.spark.mllib.optimization.ADMMUpdater
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.util.Vector
+import scala.util.Random
+import org.scalatest._
+import org.scalacheck.Gen
+import prop._
+import matchers._
+
+class ADMMSpecification extends PropSpec with GeneratorDrivenPropertyChecks {
+  val positiveInts = for (n <- Gen.choose(1, 100)) yield n
+  val shrinkageParams = for {
+    kappa <- Gen.choose(0.0, 5.0)
+    v <- Gen.choose(-100.0, 100.0)
+  } yield (kappa, v)
+
+  property("zeroes is implemented correctly") {
+    forAll (positiveInts) { (n: Int) =>
+      val x = ADMMState.zeroes(n)
+      x.length == n && x.elements.min == 0 && x.elements.max == 0
+    }
+  }
+
+  property("shrinkage is implemented correctly") {
+    forAll (shrinkageParams) { (kv: (Double, Double)) =>
+      val (kappa, v) = kv
+      val calculated =
+        if (v > kappa) v - kappa else if (math.abs(v) <= kappa) 0 else v + kappa
+
+      ADMMUpdater.shrinkage(kappa)(v) == calculated
+    }
+  }
+}

ajtulloch/spark

mllib/src/test/scala/org/apache/spark/mllib/optimization/ADMMOptimizerTest.scala
@@ -1,0 +1,53 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.mllib.classification
+
+import org.apache.spark.mllib.optimization.ADMMState
+import org.apache.spark.mllib.optimization.ADMMUpdater
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.util.Vector
+import scala.util.Random
+import org.scalatest._
+import org.scalacheck.Gen
+import prop._
+import matchers._
+
+class ADMMSpecification extends PropSpec with GeneratorDrivenPropertyChecks {
+  val positiveInts = for (n <- Gen.choose(1, 100)) yield n
+  val shrinkageParams = for {
+    kappa <- Gen.choose(0.0, 5.0)
+    v <- Gen.choose(-100.0, 100.0)
+  } yield (kappa, v)
+
+  property("zeroes is implemented correctly") {
+    forAll (positiveInts) { (n: Int) =>
+      val x = ADMMState.zeroes(n)
+      x.length == n && x.data.max == 0 && x.data.min == 0
+    }
+  }
+
+  property("shrinkage is implemented correctly") {
+    forAll (shrinkageParams) { (kv: (Double, Double)) =>
+      val (kappa, v) = kv
+      val calculated =
+        if (v > kappa) v - kappa else if (math.abs(v) <= kappa) 0 else v + kappa
+
+      ADMMUpdater.shrinkage(kappa)(v) == calculated
+    }
+  }
+}